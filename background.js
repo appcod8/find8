1a52a1cde2b5787b4e42a6475333613b204b237a
appcod8 <appcod8@gmail.com>
2025-05-09 14:29:43 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 // background.js
-// background.js
 
 // Listen for messages from the content script
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
@@ -9,11 +8,35 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   }
 });
 
-// Define a function to handle the search request
+  // Define a function to handle the search request
 function handleSearchRequest(query, url, sendResponse) {
   // Open the search result in a new tab
   chrome.tabs.create({ url: url + encodeURIComponent(query) });
   sendResponse({ message: "Search result opened in a new tab." });
+
+  // Add omnibox function
+chrome.omnibox.onInputEntered.addListener((text) => {
+  const [prefix, ...queryParts] = text.trim().split(' ');
+  const query = queryParts.join(' ');
+  let url;
+
+  switch (prefix) {
+    case 'y':
+      url = `https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`;
+      break;
+    case 'r':
+      url = `https://www.reddit.com/search/?q=${encodeURIComponent(query)}`;
+      break;
+    case 'g':
+      url = `https://github.com/search?q=${encodeURIComponent(query)}`;
+      break;
+    default:
+      url = `https://www.google.com/search?q=${encodeURIComponent(text)}`;
+  }
+
+  chrome.tabs.create({ url });
+});
+
 }
 
 // This is where you would put your background script code
